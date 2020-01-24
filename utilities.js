e664ea85f55e5cb6bd34ca01e5585714ff778503
Oleg Valter <o.a.philippov@gmail.com>
2020-01-24 17:38:15 +0300

    Added utilities and package

---
+++ utilities.js
@@ -0,0 +1,123 @@
+/**
+ * General Utilities
+ */
+
+//delays callback for a specified number of milliseconds;
+const delay = (time) => (callback) => setTimeout(callback, time || 100);
+
+//explicit no ops;
+const doNothing = (value) => value;
+
+/**
+ * String Utilities
+ */
+
+//Applies sentence case to a string
+const sentenceCase = (text) => text.charAt(0).toUpperCase() + text.slice(1);
+
+//trims whitespaces and removes non-word characters
+//example: orange, apple (!); --juice => 
+const trimAndRemoveSep = (input) => input.trim().replace(/[^\s\w]|_/g,'');
+
+/**
+ * Object Utilities
+ */
+
+//Checks if object is a valid object (not null);
+const isObject = (obj) => typeof obj === 'object' && obj !== null;
+
+//Extracts entries from an object;
+const getEntries = (object) => Object.keys(object).map(key => object[key]);
+
+//Extracts key name by its value;
+const keysByValue = (object) => (value) => Object.keys(object).filter(key => object[key] === value);
+
+/**
+ * Array Utilities
+ */
+
+//Wraps non-Array data into an Array;
+const arrayify = (data) => data instanceof Array ? data : [data];
+
+//Allocates an Array (avoids using loops);
+const allocArray = (numElems) => new Array(numElems || 0).fill(1);
+
+//Gets last element of an Array (regardless of length);
+const getLastElem = (array) => array[array.length - 1];
+
+//Gets every Nth element of an Array (optional offset);
+const getEveryNthElem = (pos) => (array,offset) => array.slice(offset||0).filter((elem, e) => !((e + 1) % pos));
+
+//Gets all elements of an Array except for one at position (composable);
+const getOtherElems = (pos) => (array) => array.filter((elem,e) => e !== pos);
+
+//Gets elements of an Array at even positions;
+const getEvenPosElems = (array) => array.filter((elem, e) => e % 2);
+
+//Gets elements of an Array at odd positions;
+const getOddPosElems = (array) => array.filter((elem, e) => !(e % 2));
+
+//Maps elements of an Array and returns only elements that are defined;
+const mapExisting = (callback) => (array) => array.map(callback).filter(e => e!==undefined);
+
+//Removes elements from start to end and returns modified Array (no mutation);
+const simpleSplice = (array) => (start,end) => array.filter((e,pos) => pos<start || pos>end);
+
+//Splits an Array on every Nth element;
+//[1,2,3,4] on second elem returns [ [1,2], [3,4] ];
+//0 as position results in an empty Array;
+const splitOnNthElem = (pos) => (array) => array
+.map((e,i,a) => !(i % pos) ? a.slice(i,i+pos) : 0)
+.filter(e => e.length);
+
+/**
+ * Date Utilities
+ */
+
+//adds number od days to a Date;
+const addDays = (days) => (date) => new Date(date.valueOf() + (days || 0) * 86400000);
+
+//splits date into ISO standard date or time string;
+const isoDate = (date) => date.toISOString().slice(0,10);
+const isoTime = (date) => date.toISOString().slice(11,19);
+
+/**
+ * Logging Utilities
+ */
+
+//logs object at log time;
+const logFixed = (object) => console.log(JSON.parse(JSON.stringify(object)));
+
+/**
+ * DOM Utilities
+ */
+
+//simply gets element by id;
+const byId = (id) => document.querySelector(`#${id}`);
+const byClass = (cls) => document.querySelector(`.${cls}`);
+
+//clears element of children (chainable);
+const clearElem = (elem) => Array.from(elem.children).forEach(child => child.remove()) || elem;
+
+//removes first child from element if it has any (non-leaking);
+const removeFirstChild = (element) => void !element.firstChild || element.firstChild.remove();
+
+//removes last child from element if it has any (non-leaking);
+const removeLastChild = (element) => void !element.lastChild || element.lastChild.remove();
+
+//removes N last children from element if it has any (non-leaking);
+const removeLastChildren = (num) => (element) => void Array.from(element.children).slice(-num).forEach(child => child.remove());
+
+//changes check state of an element + 2 use cases;
+const changeCheck = (cbx) => (value) => cbx.checked = value;
+const check = (cbx) => changeCheck(cbx)(true);
+const uncheck = (cbx) => changeCheck(cbx)(false);
+
+//toggles class on an element if it is set and vice versa (non-leaking);
+const toggleClassIfSet = (name) => (element) => void (!element.classList.contains(name) || element.classList.toggle(name));
+const toggleClassIfNot = (name) => (element) => void (element.classList.contains(name) || element.classList.toggle(name));
+
+module.exports = {
+    trimAndRemoveSep,
+    keysByValue
+};
