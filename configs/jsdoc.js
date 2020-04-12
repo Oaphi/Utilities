7d1647072b49e6c8df0969053f29d38dc7e95624
Oleg Valter <o.a.philippov@gmail.com>
2020-04-12 06:07:24 +0300

    Distributor utility script, JSDoc fixes

---
+++ configs/jsdoc.js
@@ -0,0 +1,20 @@
+/**
+ * JSDoc config for Utilities project
+ * @module
+ */
+
+module.exports = {
+    "source": {
+        "include": [
+            "src"
+        ]
+    },
+    "opts": {
+        "destination": "./docs"
+    },
+    "templates": {
+        "default": {
+            "outputSourceFiles": false
+        }
+    }
+};
