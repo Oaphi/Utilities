e339b3a247890b7fc096d230ce90238c81f89fcb
Oleg Valter <o.a.philippov@gmail.com>
2020-05-03 15:49:13 +0300

    Wrapped DOM utils into module

--- src/DOM/DOM.js
+++ src/DOM/DOM.js
@@ -1,112 +1,125 @@
 
-
-/**
- * @description traverses children left to right, calling comparator on each one
- * until it evaluates to true, then calls the callback with first element passing 
- * the condition or with root itself if none
- * @param {HTMLElement} root 
- * @param {number} [offset]
- * @param {function(HTMLElement): boolean} comparator 
- * @param {function(HTMLElement)} [callback] 
- * @param {function(HTMLElement)} [fallback]
- * @param {boolean} [strict]
- */
-function elementsRightUntil(root, offset, comparator, callback, fallback, strict = false) {
-
-    if (typeof offset === "function") {
-        fallback = callback;
-        callback = comparator;
-        comparator = offset;
-        offset = 0;
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.returnExports = factory();
     }
+}(
+    typeof self !== 'undefined' ? self : this,
+    function () {
 
-    if (typeof callback === "boolean") {
-        strict = callback;
-        callback = null;
-    }
+        /**
+         * @description traverses children left to right, calling comparator on each one
+         * until it evaluates to true, then calls the callback with first element passing 
+         * the condition or with root itself if none
+         * @param {HTMLElement} root 
+         * @param {number} [offset]
+         * @param {function(HTMLElement): boolean} comparator 
+         * @param {function(HTMLElement)} [callback] 
+         * @param {function(HTMLElement)} [fallback]
+         * @param {boolean} [strict]
+         */
+        function elementsRightUntil(root, offset, comparator, callback, fallback, strict = false) {
 
-    if (typeof fallback === "boolean") {
-        strict = fallback;
-        fallback = null;
-    }
-
-    let current = root.children[offset] || (strict ? null : root);
-
-    let matchedOnce = comparator(current) ? 1 : 0;
-
-    let index = offset;
-
-    if (!matchedOnce) {
-        while (current.nextElementSibling) {
-            index++;
-
-            current = current.nextElementSibling;
-            if (comparator(current)) {
-                matchedOnce |= 1;
-                break;
+            if (typeof offset === "function") {
+                fallback = callback;
+                callback = comparator;
+                comparator = offset;
+                offset = 0;
             }
-        }
-    }
 
-    const use = matchedOnce ? callback : fallback;
-    return use ? use(current, index) : current;
-}
-
-/**
- * @summary inverse of elementsRightUntil
- * @param {HTMLElement} root
- * @param {number} [offset]
- * @param {function(HTMLElement): boolean} comparator
- * @param {function(HTMLElement)} [callback]
- * @param {function(HTMLElement)} [fallback]
- * @param {boolean} [strict]
- */
-function elementsLeftUntil(root, offset, comparator, callback, fallback, strict = false) {
-
-    if (typeof offset === "function") {
-        fallback = callback;
-        callback = comparator;
-        comparator = offset;
-        offset = 0;
-    }
-
-    if (typeof callback === "boolean") {
-        strict = callback;
-        callback = null;
-    }
-
-    if (typeof fallback === "boolean") {
-        strict = fallback;
-        fallback = null;
-    }
-
-    const { children } = root;
-
-    const lastIndex = children.length - 1 - offset;
-
-    let current = children[lastIndex] || (strict ? null : root);
-
-    let matchedOnce = comparator(current) ? 1 : 0;
-
-    let index = lastIndex;
-
-    if (!matchedOnce) {
-        while (current.previousElementSibling) {
-            index--;
-
-            current = current.previousElementSibling;
-            if (comparator(current)) {
-                matchedOnce |= 1;
-                break;
+            if (typeof callback === "boolean") {
+                strict = callback;
+                callback = null;
             }
+
+            if (typeof fallback === "boolean") {
+                strict = fallback;
+                fallback = null;
+            }
+
+            let current = root.children[offset] || (strict ? null : root);
+
+            let matchedOnce = comparator(current) ? 1 : 0;
+
+            let index = offset;
+
+            if (!matchedOnce) {
+                while (current.nextElementSibling) {
+                    index++;
+
+                    current = current.nextElementSibling;
+                    if (comparator(current)) {
+                        matchedOnce |= 1;
+                        break;
+                    }
+                }
+            }
+
+            const use = matchedOnce ? callback : fallback;
+            return use ? use(current, index) : current;
         }
-    }
 
-    const use = matchedOnce ? callback : fallback;
-    return use ? use(current, index) : current;
-}
+        /**
+         * @summary inverse of elementsRightUntil
+         * @param {HTMLElement} root
+         * @param {number} [offset]
+         * @param {function(HTMLElement): boolean} comparator
+         * @param {function(HTMLElement)} [callback]
+         * @param {function(HTMLElement)} [fallback]
+         * @param {boolean} [strict]
+         */
+        function elementsLeftUntil(root, offset, comparator, callback, fallback, strict = false) {
 
-module.exports = {
-    elementsRightUntil,
-    elementsLeftUntil
-};
+            if (typeof offset === "function") {
+                fallback = callback;
+                callback = comparator;
+                comparator = offset;
+                offset = 0;
+            }
+
+            if (typeof callback === "boolean") {
+                strict = callback;
+                callback = null;
+            }
+
+            if (typeof fallback === "boolean") {
+                strict = fallback;
+                fallback = null;
+            }
+
+            const { children } = root;
+
+            const lastIndex = children.length - 1 - offset;
+
+            let current = children[lastIndex] || (strict ? null : root);
+
+            let matchedOnce = comparator(current) ? 1 : 0;
+
+            let index = lastIndex;
+
+            if (!matchedOnce) {
+                while (current.previousElementSibling) {
+                    index--;
+
+                    current = current.previousElementSibling;
+                    if (comparator(current)) {
+                        matchedOnce |= 1;
+                        break;
+                    }
+                }
+            }
+
+            const use = matchedOnce ? callback : fallback;
+            return use ? use(current, index) : current;
+        }
+
+        return {
+            elementsRightUntil,
+            elementsLeftUntil
+        };
+
+    }));
