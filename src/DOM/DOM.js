18d28d5e1c08dff4f7fe2524288728538ca1d83a
Oleg Valter <o.a.philippov@gmail.com>
2020-05-03 09:52:26 +0300

    Added strict mode for child traversal

--- src/DOM/DOM.js
+++ src/DOM/DOM.js
@@ -5,46 +5,95 @@
  * until it evaluates to true, then calls the callback with first element passing 
  * the condition or with root itself if none
  * @param {HTMLElement} root 
+ * @param {number} [offset]
  * @param {function(HTMLElement): boolean} comparator 
  * @param {function(HTMLElement)} [callback] 
+ * @param {function(HTMLElement)} [fallback]
+ * @param {boolean} [strict]
  */
-function elementsRightUntil(root, comparator, callback) {
+function elementsRightUntil(root, offset, comparator, callback, fallback, strict = false) {
 
-    let current = root.firstElementChild || root;
+    if (typeof offset === "function") {
+        fallback = callback;
+        callback = comparator;
+        comparator = offset;
+        offset = 0;
+    }
+
+    if (typeof callback === "boolean") {
+        strict = callback;
+        callback = null;
+    }
+
+    if (typeof fallback === "boolean") {
+        strict = fallback;
+        fallback = null;
+    }
+
+    let current = root.children[offset] || (strict ? null : root);
+
+    let matchedOnce = 0;
 
     while (current.nextElementSibling) {
         if (!comparator(current)) {
             current = current.nextElementSibling;
             continue;
         }
+        matchedOnce |= 1;
         break;
     }
 
-    return callback ? callback(current) : current;
+
+    const use = matchedOnce ? callback : fallback;
+    return use ? use(current) : current;
 }
 
 /**
  * @summary inverse of elementsRightUntil
  * @param {HTMLElement} root
+ * @param {number} [offset]
  * @param {function(HTMLElement): boolean} comparator
  * @param {function(HTMLElement)} [callback]
+ * @param {function(HTMLElement)} [fallback]
+ * @param {boolean} [strict]
  */
-function elementsLeftUntil(root, comparator, callback) {
+function elementsLeftUntil(root, offset, comparator, callback, fallback, strict = false) {
 
-    let current = root.lastElementChild || root;
+    if (typeof offset === "function") {
+        fallback = callback;
+        callback = comparator;
+        comparator = offset;
+        offset = 0;
+    }
+
+    if (typeof callback === "boolean") {
+        strict = callback;
+        callback = null;
+    }
+
+    if (typeof fallback === "boolean") {
+        strict = fallback;
+        fallback = null;
+    }
+
+    let current = root.children[root.children.length - 1 - offset] || (strict ? null : root);
+
+    let matchedOnce = 0;
 
     while (current.previousElementSibling) {
         if (!comparator(current)) {
             current = current.previousElementSibling;
             continue;
         }
+        matchedOnce |= 1;
         break;
     }
 
-    return callback ? callback(current) : current;
+    const use = matchedOnce ? callback : fallback;
+    return use ? use(current) : current;
 }
 
 module.exports = {
     elementsRightUntil,
     elementsLeftUntil
-}
+};
