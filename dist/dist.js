6f3baedd67e2f637b8bbe4983d47b5b141d1e284
Distribute Action <dist@repo.org>
2020-05-06 08:12:21 +0000

    GitHub Action: distribute

--- dist/dist.js
+++ dist/dist.js
@@ -154,77 +154,123 @@
  * @module
  */
 
-/**
- * Combines filter() and map() in O(n)
- * @param {any[]} [array]
- * @returns {function(function):function(function):any[]}
- */
-const filterMap = (array = []) => (filter = e => true) => (mapper = e => e) => {
-    const mappedArr = [];
-
-    let initialIndex = 0, filteredIndex = 0;
-
-    for (const elem of array) {
-        filter(elem, initialIndex++) &&
-            mappedArr.push(mapper(elem, filteredIndex++));
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.Arrays = factory();
     }
+}(
+    typeof self !== 'undefined' ? self : this,
+    function () {
 
-    return mappedArr;
-};
+        /**
+         * Combines filter() and map() in O(n)
+         * @param {any[]} [array]
+         * @returns {function(function):function(function):any[]}
+         */
+        const filterMap = (array = []) => (filter = e => true) => (mapper = e => e) => {
+            const mappedArr = [];
 
-/**
- * Combines filter() and map() in reverse in O(n)
- * @param {any[]} [array] 
- * @returns {function(function):function(function):any[]}
- */
-const filterMapped = (array = []) => (mapper = e => e) => (filter = e => true) => {
-    const filteredArr = [];
+            let initialIndex = 0, filteredIndex = 0;
 
-    let initialIndex = 0, filteredIndex = 0;
+            for (const elem of array) {
+                filter(elem, initialIndex++) &&
+                    mappedArr.push(mapper(elem, filteredIndex++));
+            }
 
-    for (const elem of array) {
-        const mappedElem = mapper(elem, initialIndex++);
+            return mappedArr;
+        };
 
-        filter(mappedElem, filteredIndex++) &&
-            filteredArr.push(mappedElem);
-    }
+        /**
+         * Combines filter() and map() in reverse in O(n)
+         * @param {any[]} [array] 
+         * @returns {function(function):function(function):any[]}
+         */
+        const filterMapped = (array = []) => (mapper = e => e) => (filter = e => true) => {
+            const filteredArr = [];
 
-    return filteredArr;
-};
+            let initialIndex = 0, filteredIndex = 0;
 
-/**
- * Executes a callback for each element
- * (same as forEach, but in FP style + faster)
- * @param {any[]} [array]
- * @returns {function(function):void} 
- */
-const forAll = (array = []) => (callback) => {
+            for (const elem of array) {
+                const mappedElem = mapper(elem, initialIndex++);
 
-    let index = 0;
+                filter(mappedElem, filteredIndex++) &&
+                    filteredArr.push(mappedElem);
+            }
 
-    for (const elem of array) {
-        callback(elem, index++);
-    }
+            return filteredArr;
+        };
 
-    return;
-};
+        /**
+         * Executes a callback for each element
+         * (same as forEach, but in FP style + faster)
+         * @param {any[]} [array]
+         * @returns {function(function):void} 
+         */
+        const forAll = (array = []) => (callback) => {
 
-/**
- * Maps array to values of 
- * property by key
- * @param {any[]} [array] 
- * @returns {function(string):any[]}
- */
-const keyMap = (array = []) => (key) => {
-    return !key ? array : array.map(elem => elem[key]);
-};
+            let index = 0;
 
-module.exports = {
-    filterMap,
-    filterMapped,
-    forAll,
-    keyMap
-};
+            for (const elem of array) {
+                callback(elem, index++);
+            }
+
+            return;
+        };
+
+        /**
+         * Maps array to values of 
+         * property by key
+         * @param {any[]} [array] 
+         * @returns {function(string):any[]}
+         */
+        const keyMap = (array = []) => (key) => {
+            return !key ? array : array.map(elem => elem[key]);
+        };
+
+        /**
+         * @summary merges arrays
+         * @param {any[]} source 
+         * @param  {...any[]} [targets]
+         * @returns {any[]}
+         */
+        const mergeOnto = (source, ...targets) => {
+
+            const output = [];
+
+            for(let index = 0; index < source.length; index++) {
+                const item = source[index];
+
+                if (typeof item === "undefined") {
+
+                    let finalValue = item;
+
+                    for (const target of targets) {
+                        finalValue = target[index];
+                    }
+
+                    output.push(finalValue);
+                    continue;
+                }
+        
+                output.push(item);
+            }
+
+            return output;
+        };
+
+        return {
+            filterMap,
+            filterMapped,
+            forAll,
+            keyMap,
+            mergeOnto
+        };
+
+    }));
 
 /**
  * ANDs lists of boolean values
