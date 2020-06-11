62c9493d057f163b8bcf2c51cb1dac88a8e2cee9
Distribute Action <dist@repo.org>
2020-06-11 08:00:06 +0000

    GitHub Action: distribute

--- dist/dist.js
+++ dist/dist.js
@@ -935,288 +935,311 @@ module.exports = {
     sum
 };
 
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        root.Objects = factory();
+
+/**
+ * @summary returns a complement of objects
+ * @param  {...object} sources 
+ * @returns {object}
+ */
+const complement = (...sources) => {
+
+    const tracked = [];
+
+    return sources.reduce((acc, curr) => {
+
+        for (const key in curr) {
+
+            if (tracked.includes(key)) {
+                delete acc[key];
+                continue;
+            }
+
+            acc[key] = curr[key];
+
+            tracked.push(key);
+        }
+
+        return acc;
+    }, {});
+};
+
+/**
+ * @summary deep gets properties of specified type
+ * @param {object} [obj]
+ */
+const deepGetByType = (obj = {}) =>
+
+    /**
+     * @param {string} type
+     * @returns {object}
+     */
+    (type) => {
+
+        const output = {};
+
+        Object
+            .entries(obj)
+            .forEach(([key, val]) => {
+
+                if (typeof val === "object" && val) {
+                    const subvals = deepGetByType(val)(type);
+                    return Object.assign(output, subvals);
+                }
+
+                const shouldSet = typeof val === type ||
+                    type === null && val === null;
+
+                if (shouldSet) {
+                    output[key] = val;
+                }
+
+            });
+
+        return output;
+    };
+
+/**
+ * @typedef {({ 
+ *  configurable: boolean,
+ *  enumerable: boolean,
+ *  writable: boolean,
+ *  get: function, 
+ *  set: function,
+ *  value: any
+ * })} PropertyDescriptor
+ * 
+ * @param {object} obj
+ * @returns {PropertyDescriptor}
+ */
+const getGetterDescriptors = (obj = {}) => {
+    return Object
+        .entries(Object.getOwnPropertyDescriptors(obj))
+        .filter(entry => typeof entry[1].get === "function");
+};
+
+/**
+ * @summary gets value from object or inits it via callback
+ * @param {object} obj
+ * @param {string} propName
+ * @param {function(object) : any} [callback]
+ * @returns {any}
+ */
+const getOrInitProp = (obj, propName, callback) => {
+
+    if (propName in obj) {
+        return obj[propName];
     }
-}(
-    typeof self !== 'undefined' ? self : this,
-    function () {
 
+    if (callback) {
+        obj[propName] = callback(obj);
+        return obj[propName];
+    }
+};
 
-        /**
-         * @summary returns a complement of objects
-         * @param  {...object} sources 
-         * @returns {object}
-         */
-        const complement = (...sources) => {
+/**
+ * @summary checks if an object is a valid object
+ * @param {object} obj
+ * @returns {boolean}
+ */
+const isObject = (obj) => typeof obj === 'object' && obj !== null && !Array.isArray(obj);
 
-            const tracked = [];
+/**
+ * @summary pushes value in or inits array with value
+ * @param {object} obj 
+ * @param {string} key 
+ * @param {*} value 
+ * @returns {object}
+ */
+const pushOrInitProp = (obj, key, value) => {
 
-            return sources.reduce((acc, curr) => {
+    if (key in obj) {
+        const temp = obj[key];
 
-                for (const key in curr) {
-
-                    if (tracked.includes(key)) {
-                        delete acc[key];
-                        continue;
-                    }
-
-                    acc[key] = curr[key];
-
-                    tracked.push(key);
-                }
-
-                return acc;
-            }, {});
-        };
-
-        /**
-         * @typedef {({ 
-         *  configurable: boolean,
-         *  enumerable: boolean,
-         *  writable: boolean,
-         *  get: function, 
-         *  set: function,
-         *  value: any
-         * })} PropertyDescriptor
-         * 
-         * @param {object} obj
-         * @returns {PropertyDescriptor}
-         */
-        const getGetterDescriptors = (obj = {}) => {
-            return Object
-                .entries(Object.getOwnPropertyDescriptors(obj))
-                .filter(entry => typeof entry[1].get === "function");
-        };
-
-        /**
-         * @summary gets value from object or inits it via callback
-         * @param {object} obj
-         * @param {string} propName
-         * @param {function(object) : any} [callback]
-         * @returns {any}
-         */
-        const getOrInitProp = (obj, propName, callback) => {
-
-            if (propName in obj) {
-                return obj[propName];
-            }
-
-            if (callback) {
-                obj[propName] = callback(obj);
-                return obj[propName];
-            }
-        };
-
-        /**
-         * @summary checks if an object is a valid object
-         * @param {object} obj
-         * @returns {boolean}
-         */
-        const isObject = (obj) => typeof obj === 'object' && obj !== null && !Array.isArray(obj);
-
-        /**
-         * @summary pushes value in or inits array with value
-         * @param {object} obj 
-         * @param {string} key 
-         * @param {*} value 
-         * @returns {object}
-         */
-        const pushOrInitProp = (obj, key, value) => {
-
-            if (key in obj) {
-                const temp = obj[key];
-
-                if (Array.isArray(temp)) {
-                    temp.push(value);
-                    return obj;
-                }
-
-                obj[key] = [temp, value];
-                return obj;
-            }
-
-            obj[key] = [value];
-
+        if (Array.isArray(temp)) {
+            temp.push(value);
             return obj;
-        };
+        }
 
-        /**
-         * @typedef {object} SetOptions
-         * @property {string} [coerce]
-         * 
-         * 
-         * @summary copies property if it exists
-         * @param {object} source
-         * @param {string} key 
-         * @param {object} [target]
-         * @param {SetOptions} [options]
-         * @returns {object}
-         */
-        const setIf = (source, key, target = {}, options = {}) => {
+        obj[key] = [temp, value];
+        return obj;
+    }
 
-            if (typeof key !== "string") {
-                return target;
+    obj[key] = [value];
+
+    return obj;
+};
+
+/**
+ * @typedef {object} SetOptions
+ * @property {string} [coerce]
+ * 
+ * 
+ * @summary copies property if it exists
+ * @param {object} source
+ * @param {string} key 
+ * @param {object} [target]
+ * @param {SetOptions} [options]
+ * @returns {object}
+ */
+const setIf = (source, key, target = {}, options = {}) => {
+
+    if (typeof key !== "string") {
+        return target;
+    }
+
+    /** @type {Map<string, function>} */
+    const coercionMap = new Map()
+        .set("string", val => {
+
+            if (typeof val === "string") {
+                return val;
             }
 
-            /** @type {Map<string, function>} */
-            const coercionMap = new Map()
-                .set("string", val => {
-
-                    if (typeof val === "string") {
-                        return val;
-                    }
-
-                    if (typeof val === "number") {
-                        return Number.prototype.toString.call(val);
-                    }
-
-                    return val;
-                });
-
-            const { coerce } = options;
-
-            if (key in source) {
-                const value = source[key];
-
-                target[key] = coerce ? (coercionMap.get(coerce))(value) : value;
+            if (typeof val === "number") {
+                return Number.prototype.toString.call(val);
             }
 
-            return target;
-        };
+            return val;
+        });
 
-        /**
-         * @summary defines a smart (memoizable) getter
-         * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get}
-         * @param {object} context 
-         * @param {string} propName 
-         * @param {function} callback 
-         * @returns {object}
-         */
-        const smartGetter = (context, propName, callback) => {
-            return Object.defineProperty(context, propName, {
-                configurable: true,
-                get() {
-                    const temp = callback(context);
+    const { coerce } = options;
 
-                    Object.defineProperty(context, propName, {
-                        value: temp
-                    });
+    if (key in source) {
+        const value = source[key];
 
-                    return temp;
-                }
-            });
-        };
+        target[key] = coerce ? (coercionMap.get(coerce))(value) : value;
+    }
 
-        /**
-         * @summary returns one of the object props equal
-         * @param {object} [target] first object to compare
-         * @param {string} propName property name
-         */
-        const switchIfDiffProp = (target, propName) =>
+    return target;
+};
 
-            /**
-             * @param {object} [source] second object to compare
-             * @returns {object}
-             */
-            (source) => {
+/**
+ * @summary defines a smart (memoizable) getter
+ * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get}
+ * @param {object} context 
+ * @param {string} propName 
+ * @param {function} callback 
+ * @returns {object}
+ */
+const smartGetter = (context, propName, callback) => {
+    return Object.defineProperty(context, propName, {
+        configurable: true,
+        get() {
+            const temp = callback(context);
 
-                if (!target) {
-                    return source || {};
-                }
-
-                if (!source) {
-                    return target || {};
-                }
-
-                return target[propName] === source[propName] ?
-                    target :
-                    source;
-            };
-
-        /**
-         * @summary makes a union of object
-         * @param {object} target 
-         * @param  {...object} sources 
-         * @returns {object}
-         */
-        const union = (target, ...sources) => {
-
-            const union = Object.assign({}, target);
-
-            const assignedKeys = {};
-
-            for (const source of sources) {
-                for (const key in source) {
-                    if (!assignedKeys[key] && !(key in union)) {
-                        assignedKeys[key] |= 1;
-                        union[key] = source[key];
-                    }
-                }
-            }
-
-            return union;
-        };
-
-        /**
-         * @summary checks which key is set on object
-         * @param {object} obj 
-         * @param  {...string} [keys] 
-         * @returns {?string}
-         * 
-         * @throws {RangeError}
-         */
-        const whichKeyIsSet = (obj, ...keys) => {
-
-            let matched = 0;
-
-            const filtered = keys.filter(key => {
-                const has = obj.hasOwnProperty(key);
-
-                if (has) {
-
-                    if (matched) {
-                        throw new RangeError(`Object has more than one provided own keys`);
-                    }
-
-                    matched |= 1;
-                }
-
-                return has;
+            Object.defineProperty(context, propName, {
+                value: temp
             });
 
-            return filtered[0] || null;
-        };
+            return temp;
+        }
+    });
+};
 
-        /**
-         * @summary checks which keys are set on object
-         * @param {object} obj 
-         * @param  {...string} [keys] 
-         * @returns {string[]}
-         */
-        const whichKeysAreSet = (obj, ...keys) => {
-            return keys.filter(key => obj.hasOwnProperty(key));
-        };
+/**
+ * @summary returns one of the object props equal
+ * @param {object} [target] first object to compare
+ * @param {string} propName property name
+ */
+const switchIfDiffProp = (target, propName) =>
 
-        return {
-            complement,
-            getGetterDescriptors,
-            getOrInitProp,
-            isObject,
-            pushOrInitProp,
-            setIf,
-            smartGetter,
-            switchIfDiffProp,
-            union,
-            whichKeyIsSet,
-            whichKeysAreSet
-        };
-    }));
+    /**
+     * @param {object} [source] second object to compare
+     * @returns {object}
+     */
+    (source) => {
+
+        if (!target) {
+            return source || {};
+        }
+
+        if (!source) {
+            return target || {};
+        }
+
+        return target[propName] === source[propName] ?
+            target :
+            source;
+    };
+
+/**
+ * @summary makes a union of object
+ * @param {object} target 
+ * @param  {...object} sources 
+ * @returns {object}
+ */
+const union = (target, ...sources) => {
+
+    const union = Object.assign({}, target);
+
+    const assignedKeys = {};
+
+    for (const source of sources) {
+        for (const key in source) {
+            if (!assignedKeys[key] && !(key in union)) {
+                assignedKeys[key] |= 1;
+                union[key] = source[key];
+            }
+        }
+    }
+
+    return union;
+};
+
+/**
+ * @summary checks which key is set on object
+ * @param {object} obj 
+ * @param  {...string} [keys] 
+ * @returns {?string}
+ * 
+ * @throws {RangeError}
+ */
+const whichKeyIsSet = (obj, ...keys) => {
+
+    let matched = 0;
+
+    const filtered = keys.filter(key => {
+        const has = obj.hasOwnProperty(key);
+
+        if (has) {
+
+            if (matched) {
+                throw new RangeError(`Object has more than one provided own keys`);
+            }
+
+            matched |= 1;
+        }
+
+        return has;
+    });
+
+    return filtered[0] || null;
+};
+
+/**
+ * @summary checks which keys are set on object
+ * @param {object} obj 
+ * @param  {...string} [keys] 
+ * @returns {string[]}
+ */
+const whichKeysAreSet = (obj, ...keys) => {
+    return keys.filter(key => obj.hasOwnProperty(key));
+};
+
+export {
+    complement,
+    deepGetByType,
+    getGetterDescriptors,
+    getOrInitProp,
+    isObject,
+    pushOrInitProp,
+    setIf,
+    smartGetter,
+    switchIfDiffProp,
+    union,
+    whichKeyIsSet,
+    whichKeysAreSet
+};
 
 /**
  * Generates pseudo-random int[]
