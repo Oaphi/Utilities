76f285e7bf292e2860ef985f5f5ca3b568736377
Distribute Action <dist@repo.org>
2020-08-18 23:38:13 +0000

    GitHub Action: distribute

--- dist/dist.js
+++ dist/dist.js
@@ -391,7 +391,15 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
 
@@ -656,6 +664,12 @@ var reduceWithStep = function reduceWithStep(_ref2) {
 /**
  * @typedef {object} ShrinkConfig
  * @property {any[][]} [source]
+ * @property {{ 
+ *  top : number, 
+ *  right : number, 
+ *  bottom : number, 
+ *  left : number 
+ * }} [leave]
  * @property {number} [left]
  * @property {number} [right]
  * @property {number} [bottom]
@@ -795,9 +809,93 @@ var splitIntoConseq = function splitIntoConseq() {
   });
   return sequences;
 };
+/**
+ * @summary creates an object counter
+ * @param {{
+ *  onKey : string,
+ *  source : object[]
+ * }}
+ */
+
+
+var countObjects = function countObjects() {
+  var _ref4 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+      _ref4$source = _ref4.source,
+      source = _ref4$source === void 0 ? [] : _ref4$source,
+      onKey = _ref4.onKey;
+
+  var validObjects = source.filter(Boolean);
+  var length = validObjects.length;
+
+  if (!length) {
+    return {};
+  }
+
+  var validProp = onKey || Object.keys(validObjects[0])[0];
+  var counter = {};
+  validObjects.forEach(function (obj) {
+    if (validProp in obj) {
+      var val = obj[validProp];
+      var inCount = counter[val] || 0;
+      counter[val] = inCount + 1;
+    }
+  });
+  return counter;
+};
+/**
+ * @typedef {{
+ *  source : object[],
+ *  type : ("entries"|"keys"|"values")
+ * }} DedupeConfig
+ * 
+ * @param {DedupeConfig}
+ * @returns {object[]}
+ */
+
+
+var deduplicate = function deduplicate() {
+  var _ref5 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+      _ref5$source = _ref5.source,
+      source = _ref5$source === void 0 ? [] : _ref5$source,
+      _ref5$type = _ref5.type,
+      type = _ref5$type === void 0 ? "entries" : _ref5$type;
+
+  var toDedupe = source.map(function (obj) {
+    return obj;
+  }).reverse();
+  var length = toDedupe.length;
+  return source.filter(function (srcObj, srcIdx) {
+    var srcEntries = Object.entries(srcObj);
+    var lastIdx = toDedupe.findIndex(function (tgtObj) {
+      var tgtEntries = Object.entries(tgtObj);
+
+      if (tgtEntries.length !== srcEntries.length) {
+        return false;
+      }
+
+      var sameOnEntries = type === "entries" && tgtEntries.every(function (_ref6) {
+        var _ref7 = _slicedToArray(_ref6, 2),
+            key = _ref7[0],
+            val = _ref7[1];
+
+        return srcObj[key] === val;
+      });
+      var sameOnValues = type === "values" && Object.values(tgtObj).every(function (tgtVal) {
+        return Object.values(srcObj).includes(tgtVal);
+      });
+      var sameOnKeys = type === "keys" && Object.keys(tgtObj).every(function (tgtKey) {
+        return Object.keys(srcObj).includes(tgtKey);
+      });
+      return sameOnEntries || sameOnValues || sameOnKeys;
+    });
+    return srcIdx === length - lastIdx - 1;
+  });
+};
 
 var _default = {
   chunkify: chunkify,
+  countObjects: countObjects,
+  deduplicate: deduplicate,
   filterMap: filterMap,
   filterMapped: filterMapped,
   forAll: forAll,
