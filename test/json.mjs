97cce7601be1fb3198d267f6726100e818c8d43a
Oleg Valter <o.a.philippov@gmail.com>
2020-09-03 09:38:40 +0300

    Added tests for the new query utility

--- test/json.mjs
+++ test/json.mjs
@@ -1,7 +1,57 @@
 import chai from "chai";
 const { expect } = chai;
 
-import { JSONtoQuery } from "../src/JSON/json.mjs";
+import { JSONtoQuery, objectToQuery } from "../src/JSON/json.mjs";
+
+describe('JSON', function () {
+
+    describe('objectToQuery', function () {
+
+        it('should convert simple values correctly', function () {
+            const source = { num: 1, bool: false, str: "test" };
+            const output = objectToQuery(source);
+            expect(output).to.equal("num=1&bool=false&str=test");
+        });
+
+        it('should remove null and undefined values', function () {
+            const source = { nullish: null, undefish: undefined, ok: 200 };
+            const output = objectToQuery(source);
+            expect(output).to.equal("ok=200");
+        });
+
+        it('should convert objects to bracket notation by default', function () {
+            const source = { obj: { one: 1, two: 2 } };
+            const output = objectToQuery(source);
+            expect(output).to.equal("obj[one]=1&obj[two]=2");
+        });
+
+        it('should convert objects to dot notation with "dot" type', function () {
+            const source = { obj: { one: 1, two: 2 } };
+            const output = objectToQuery(source, { objectNotation: "dot" });
+            expect(output).to.equal("obj.one=1&obj.two=2");
+        });
+
+        it('should convert arrays same as objects by default', function () {
+            const source = { test: [1, 2, 3] };
+            const output = objectToQuery(source);
+            expect(output).to.equal("test[0]=1&test[1]=2&test[2]=3");
+        });
+
+        it('should convert arrays to empty bracket notation with "empty_bracket" type', function () {
+            const source = { test: [1, 2, 3] };
+            const output = objectToQuery(source,{ arrayNotation: "empty_bracket" });
+            expect(output).to.equal("test[]=1&test[]=2&test[]=3");
+        });
+
+        it('should convery arrays to comma notation with "comma" type', function () {
+            const source = { obj : [ true, false ] };
+            const output = objectToQuery(source, { arrayNotation: "comma" });
+            expect(output).to.equal("obj=true,false");
+        });
+
+    });
+
+});
 
 describe('JSONtoQuery', function () {
 
@@ -70,7 +120,7 @@ describe('JSONtoQuery', function () {
                 one: 1,
                 two: 2,
                 three: 3,
-                doesNotCount : 999
+                doesNotCount: 999
             }, {
                 paramOrder: countDown
             });
