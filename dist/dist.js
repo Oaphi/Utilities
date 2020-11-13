9cd80b158f6350144b142784677b6240ee3019cd
Distribute Action <dist@repo.org>
2020-11-13 02:01:52 +0000

    GitHub Action: distribute

--- dist/dist.js
+++ dist/dist.js
@@ -4080,7 +4080,11 @@ module.exports = {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.trimAndRemoveSep = exports.splitIntoSentences = exports.sentenceCase = exports.pluralizeCountable = exports.parToSentenceCase = exports.isUcase = exports.isLcase = void 0;
+exports.trimAndRemoveSep = exports.splitIntoSentences = exports.sentenceCase = exports.getJoinedEntityList = exports.pluralizeCountable = exports.parToSentenceCase = exports.isUcase = exports.isLcase = void 0;
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
@@ -4353,8 +4357,29 @@ var parToSentenceCase = function parToSentenceCase() {
   });
   return normalized.join(" ");
 };
+/**
+ * @summary joins a list of entities ("a,b, and c")
+ * @param {string[]} entities 
+ */
+
 
 exports.parToSentenceCase = parToSentenceCase;
+
+var getJoinedEntityList = function getJoinedEntityList(entities) {
+  var length = entities.length;
+  var addComma = length > 2;
+
+  var _entities = _slicedToArray(entities, 1),
+      first = _entities[0];
+
+  if (length === 1) {
+    return first;
+  }
+
+  return "".concat(entities.slice(0, -1).join(", ")).concat(addComma ? "," : "", " and ").concat(entities.slice(-1)[0]);
+};
+
+exports.getJoinedEntityList = getJoinedEntityList;
 "use strict";
 
 function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
